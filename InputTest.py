625bcb6803e944ca89b84765be94d595cf643a55
scout205 <scout205@comcast.net>
2016-09-27 16:37:10 -0400

    Basic Template
    Added imports and general arguement statement

--- InputTest.py
+++ InputTest.py
@@ -1,3 +1,18 @@
 ## Test for taking in the input and setting up variables
 
+#Imports
+import math
+import random
+import numpy as np
+import csv
+import sys
+import re
+#from StringIO import StringIO
+
+
+#arguements from input .csv file
+script, inPut = sys.argv
+
+#dataT = np.loadtxt(open(csvIN,"rb"),delimiter=",",skiprows=1)
+
 
