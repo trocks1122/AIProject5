a83c27425b416c9590a9a3bb7c7c3f82734194b7
Alex Ruggiero <scout205@comcast.net>
2016-09-28 00:20:46 -0400

    Psudo code
    Added psudo code for the input

--- InputTest.py
+++ InputTest.py
@@ -14,5 +14,62 @@ import re
 script, inPut = sys.argv
 
 #dataT = np.loadtxt(open(csvIN,"rb"),delimiter=",",skiprows=1)
+ln = ""
+lx = 0
+ly = 0
+counter = 0
+fr = open(inPut, R)
+for line in fr:
+    #print line
+    #if line contains "#":
+        counter++
+    #else
+        if counter==0:
+            #NO start header, skip
+        elif counter==1:
+            #if line contains "A":
+                #A is active, set weight
+            #elif line contains "B":
+                #B is active, set weight
+            #elif line contains "C":
+                #C is active, set weight
+            #...
+            #elif line contains "Z":
+                #Z is active, set weight
+        elif counter == 2:
+            #if line contains "a":
+                #a is active, set weight
+            #elif line contains "b":
+                #b is active, set weight
+            #elif line contains "c":
+                #c is active, set weight
+            #...
+            #elif line contains "z":
+                #z is active, set weight     
+       elif counter == 3:
+           #lx = x
+           #ly = y
+       elif counter == 4:
+           #set inclusive contraints on items
+       elif counter == 5:
+           #set exclusive contraints on items
+       elif counter == 6:
+           #Set items to be in same bag
+       elif counter == 7:
+           #set items to never be in the same bag
+       elif counter == 8:
+           #set items and bags to be mutally inclusive
+    
+    f.closed
+
+       
+
+
+
+
+     
+
+
+
 
 
