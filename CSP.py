4c79153951e1037b99eee56929fb6582e3a0723e
trocks1122 <trocks1122@gmail.com>
2016-10-02 16:50:27 -0400

    added more to CSP implementation

--- CSP.py
+++ CSP.py
@@ -5,6 +5,46 @@ import numpy as np
 import search
 import types
 
+class CSP(search.problem):
+	def init(self, vars, domains, neighbors, constraints):
+		vars = vars or domains.keys()
+		update(slef, vars=vars, domains=domains, neighbors=neighbors, initial={}, curr_domains=None, pruned=None, nassigns=0)
+
+	def assign(self, var, val assignment):
+		self.nassign += 1
+		assignment[var] = 1 
+		if self.curr_domains:
+			if self.fc:
+				self.forward_check(var, val, assignment)
+			if self.mac:
+				Arc_Consistent(self, [(xk, var) for xk in self.neighbors[var]])
+
+	def unassign(self, var, assignment):
+		if var in assignment:
+			if self.curr_domains:
+				self.curr_domains[var] = self.domains[var][:]
+			del assignment[var]
+
+	def conflict(self, var, val, assignment):
+		testVal = assignment.get
+
+	def forwardCheck(self, var, val, assignment):
+		#performs forward checking on given assignment
+		if self.curr_domains:
+			for (i, h) in self.pruned[var]:
+				self.curr_domains[i].append(h)
+			self.pruned[var] = []
+			for i in self.neighbors[var]:
+				if i not in assignment:
+					if h in self.curr_domains[i][:]:
+						if not self.constraint(var, val, i, h):
+							self.curr_domains[i].remove[h]
+							self.pruned[var].append((i, h))
+
+
+
+
+
 def backtracking(csp, mcv=False, lcv=False, fc=False, mac=False):
 	if fc or mac:
 		csp.curr_domains, csp.pruned = {}, {}
@@ -14,7 +54,7 @@ def backtracking(csp, mcv=False, lcv=False, fc=False, mac=False):
 	update(csp, mcv=mcv,lcv=lcv,fc=fc,mac=mac)
 
 
-def recursive_backtracking(assign, csp):
+def recursive_backtracking(assignment, csp):
 	if len(assign) == len(csp.vars):
 		return assign
 	var = select_unassigned_variable(assignment, csp)
@@ -54,3 +94,20 @@ def num_good_vals(csp, var, assignment):
 		return count_if(lambda val: csp.nconflicts(var, val,assignment) == 0, csp.domains[var])
 
 
+def Arc_Consistent(csp, queue=None):
+	#from figure 6.3
+	if queue == None:
+		queue = [(i,h) for i in csp.vars for h in csp.neighbors[i]]
+	while queue:
+		(i, j) = queue.pop()
+		if remove_bad_vals(csp, i, j):
+			for h in csp.neighbors[i]:
+				queue.append((h, i))
+
+def remove_bad_vals(csp, i, j):
+	removed = False
+	for x in csp.curr_domains[i][:]:
+		if every(lambda y: not csp.constraints(i, x, j, y), csp.curr_domains[j]):
+			csp.curr_domains[i].remove(x)
+			removed = true
+	return removed
