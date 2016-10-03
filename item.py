1f9043951012d5527c22d7bc0d73fe4fa0f233dc
Alex Ruggiero <scout205@comcast.net>
2016-10-02 23:21:20 -0400

    Modified item.py
    Added missing functions and fix spaces

--- item.py
+++ item.py
@@ -1,14 +1,25 @@
 class Item(object):
-	def __init__(self, name, weight):
-		#item has a name and weight, no bag but has potential bags and has constramits
-		self.name = name
-		self.weight = int(weight) #convert to int
-		self.bag = None
-		self.potential_bags = {}
-		self.constraints = []
+    def __init__(self, name, weight):
+    #item has a name and weight, no bag but has potential bags and has constramits
+    self.name = name
+    self.weight = int(weight) #convert to int
+    self.bag = None
+    self.potential_bags = {}
+    self.constraints = []
 
-	def putInBag(self, bag):
-		if self.bag:
-			self.bag.items = [s for s in self.bag.items if s.name is not self.name]
-			bag.items.append(self)
-			self.bag = bag
+    def __eq__(self, other):
+        if isinstance(other, Item):
+            return self.name == other.name
+        return NotImplemented
+
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
+
+    def putInBag(self, bag):
+        if self.bag:
+            self.bag.items = [s for s in self.bag.items if s.name is not self.name]
+        bag.items.append(self)
+        self.bag = bag
